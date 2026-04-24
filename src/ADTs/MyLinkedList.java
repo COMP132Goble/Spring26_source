e35b5f8af1fd9b7eb49fdcb2ee577b017f2bfcf6
wpgoble <will.goble@gmail.com>
2026-04-24 10:19:51 -0400

    adds skeletal code for get, set, and remove methods

--- src/ADTs/MyLinkedList.java
+++ src/ADTs/MyLinkedList.java
@@ -14,14 +14,30 @@ public class MyLinkedList {
     public static void main(String[] args) {
         MyLinkedList myList = new MyLinkedList();
 
-        System.out.println(myList);
-        myList.addFront("A");
-        System.out.println(myList);
-        myList.addFront("B");
-        System.out.println(myList);
-        myList.addFront("C");
+        /*
+            Let's just populate a list of random characters
+        */
+        char val = 'A';
+        for(int i = 0; i < 10; i++) {
+            if (i % 2 == 1){
+                val++;
+                continue;
+            }
+            String value = String.valueOf(val);
+            val++;
+            myList.add(value);
+            System.out.println(myList);
+        }
+        myList.add(1, "B");
 
         System.out.println(myList);
+
+        // int index = 3;
+        // String value = myList.get(index);
+
+        // System.out.println("The value at index " 
+        //     + index + " is " + value
+        // );
     }
 
     public void addFront(String value) {
@@ -44,6 +60,89 @@ public class MyLinkedList {
         size++;
     }
 
+    /** 
+     * Notice that for the add method for the List ADT we are just using the 
+     * addBack method, so by default the List data structure adds things to 
+     * the end.
+     * */
+    public void add(String value) {
+        Node newNode = new Node(value);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    /**
+     * Now we want the ability to insert a node at a particular index.
+     * First we want to verify that we were given a valid index, then 
+     * we will want to traverse the list until we are right before the index we 
+     * want our value at. This is because when we add the element it will be placed 
+     * at the desired index.
+     *
+     * @param index where we would like our new value to be placed
+     * @param value the string that we want stored in our list.
+     * @throws IndexOutOfBoundsException
+     */
+    public void add(int index, String value) throws IndexOutOfBoundsException {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("tried to set " 
+                    + index + " but size is " + size + ".");
+        } 
+
+        if(index == 0) {
+            addFront(value);
+        } else {
+            Node newNode = new Node(value);
+            Node previous = head;
+            /*
+            Starting at the head, we will traverse until we are at the node that
+            points to the desired index.
+            */
+            for(int i = 0; i < index-1; i++) {
+                previous = previous.next;
+            }
+
+            newNode.next = previous.next;
+            previous.next = newNode;
+            size++;
+        }
+    }
+    
+    public String get(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(
+                "tried to access index " + index + " but the size" 
+                + " is " + size 
+            );
+        }
+
+        return "";
+    }
+
+    public void set(int index, String value) throws IndexOutOfBoundsException {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(
+                "tried to access index " + index + " but the size" 
+                + " is " + size 
+            );
+        }
+    }
+
+    public String remove(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(
+                "tried to access index " + index + " but the size" 
+                + " is " + size 
+            );
+        }
+        return "";
+    }
 
     public int size() {
         return size;
